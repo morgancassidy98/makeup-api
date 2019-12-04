0b5c7da18e988007a299f5b0c33c5dda4e6a3ada
Cassidy Morgan <morgancassidy98@gmail.com>
2019-12-04 18:56:05 -0500

    Updates

---
+++ precache-manifest.9348e5ef17eca1b8b14be05221ec6cf8.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3d2b7b1a6bb92b9174d3fbecc53ba142",
+    "url": "https/morgancassidy98.github.io/makeup-api/index.html"
+  },
+  {
+    "revision": "6b9f3e74b58b460020de",
+    "url": "https/morgancassidy98.github.io/makeup-api/static/css/main.2c7f8e33.chunk.css"
+  },
+  {
+    "revision": "a5376a362e6c69af6c59",
+    "url": "https/morgancassidy98.github.io/makeup-api/static/js/2.d090816b.chunk.js"
+  },
+  {
+    "revision": "6b9f3e74b58b460020de",
+    "url": "https/morgancassidy98.github.io/makeup-api/static/js/main.e0bd23cf.chunk.js"
+  },
+  {
+    "revision": "d7f5044d16a2d640229b",
+    "url": "https/morgancassidy98.github.io/makeup-api/static/js/runtime~main.44e4eeb3.js"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "https/morgancassidy98.github.io/makeup-api/precache-manifest.9348e5ef17eca1b8b14be05221ec6cf8.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("https/morgancassidy98.github.io/makeup-api/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
+});
